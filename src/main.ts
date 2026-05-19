a65f019d8e09a95499559135fdb20c183646b210
Berta González <berta.gonzalez99@gmail.com>
2026-05-19 17:08:05 +0200

    nav-bar and main.ts changed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
+import { navBar } from './components/nav-bar';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
-
-`
+${navBar()}
+  
+  <main id="main-content">
+    </main>
+`;
