cf1e9100ec50c9b3995f71f32feaaf615ec03378
Berta González <berta.gonzalez99@gmail.com>
2026-05-20 13:05:03 +0200

    feat(home): Add first section hero

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
+import { pagesHome } from './pages/home/home'
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
+${pagesHome()}
+  
+  <main id="main-content">
+    </main>
 
-`
+`;
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,1 +1,9 @@
 
+import "/Especialització_ItAcademy/PROJECTE 1 ITAlumni/first-itacademy-project-berta/src/pages/home/home.css";
+
+export function pagesHome(): string {
+
+    return `
+    
+    `;
+}
