3af81a483c398399bc69ecfd7125a53f545d8742
Berta González <berta.gonzalez99@gmail.com>
2026-05-26 10:07:18 +0200

    fix(dev): Add tab-bar

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import './style.css'
 import { navBar } from './components/nav-bar'
 import { statusBar } from './components/status-bar/status-bar'
 import { footer } from './components/footer/footer';
+import { tabBar } from './components/tab-bar/tab-bar';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 ${navBar()}
 ${statusBar()}
 ${footer()}
+${tabBar()}
   
   <main id="main-content">
     </main>
