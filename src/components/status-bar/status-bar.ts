6de00745f02fdf597eade472934ec3cd0871c60e
Berta González <berta.gonzalez99@gmail.com>
2026-05-26 09:41:58 +0200

    fix(status-bar): Add main and finish styles

---
+++ src/components/status-bar/status-bar.ts
@@ -0,0 +1,13 @@
+
+import "/Especialització_ItAcademy/PROJECTE 1 ITAlumni/first-itacademy-project-berta/src/components/status-bar/status-bar.css";
+
+export function statusBar(): string {
+
+    return `
+    <div class="status-bar">
+      <img class="status-bar__time" src="src/assets/icons/status-bar//hour.svg" alt="time icon">
+      <img class="status-bar__icons" src="src/assets/icons/status-bar/battery-wifi.svg" alt="status icons">
+      </div>
+      </div>
+    `;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './style.css'
+import { statusBar } from './components/status-bar/status-bar'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
+${statusBar()}
+
+ <main id="main-content">
+    </main>
 
 `
