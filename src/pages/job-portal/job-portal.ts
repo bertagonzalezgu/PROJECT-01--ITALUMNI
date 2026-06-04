ec4df207bd674143bf9a61c72ee6951c6defa3bd
Berta González <berta.gonzalez99@gmail.com>
2026-06-04 13:22:39 +0200

    fix(data-jobs): Add jobs database ans adapt jobs.ts

--- src/pages/job-portal/job-portal.ts
+++ src/pages/job-portal/job-portal.ts
@@ -4,86 +4,27 @@ import { jobs } from '../../data/jobs';
 export function pagesJobsDesktop(): string{
 
     const jobCards = jobs.map(job => `
-    <div class="card" data-title="${job.title.toLowerCase()}" data-type="${job.type.toLowerCase()}">
-      <div class="card-body__text">
-        <h3>${job.title}</h3>
-        <p>${job.type}</p>
-        <p>${job.date}</p>
-        <button class="btn btn-primary">Aplica Ara</button>
+      <div class="card" data-title="${job.title.toLowerCase()}" data-type="${job.type.toLowerCase()}">
+        <div class="card-body__text">
+          <h3>${job.title}</h3>
+          <p>${job.type}</p>
+          <p>Publicat ${job.date}</p>
+          <button class="btn btn-primary">Aplica Ara</button>
+        </div>
+        <img class="card-body__image" src="${job.image}" alt="${job.alt}">
       </div>
-    </div>
     `).join('');
 
     return `
-    <div class="container">
+      <div class="container">
         
           <h1 class="cards-grid__title">Ofertes de feina</h1>
 
         <section class="cards-grid">
 
-          <div class="card">
-            <div class="card-body__text">
-              <h3>Enginyer de Software</h3>
-              <p>Jornada completa</p>
-              <p>Publicat fa 3 dies</p>
-              <button class="btn btn-primary">Aplica Ara</button>
-            </div>
-            
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/software-enginyer.jpg" alt="computer on a desk">
-          
-          </div>
+          ${jobCards}
 
-          <div class="card">
-            <div class="card-body__text">
-              <h3>Mànager de producte</h3>
-              <p>Oportunitat en remot</p>
-              <p>Publicat fa 1 setmana</p>
-              <button class="btn btn-primary">Més Info</button>
-            </div>
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/product.jpg" alt="architecture design">
-          </div>
-
-          <div class="card">
-            <div class="card-body__text">
-              <h3>Analista Data</h3>
-              <p>Rol amb contracte</p>
-              <p>Publicat fa 2 dies</p>
-              <button class="btn btn-primary">Més Detalls</button>
-            </div>
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/data.jpg" alt="professor in a class">
-          </div>
-
-          <div class="card">
-            <div class="card-body__text">
-              <h3>UX Designer</h3>
-              <p>Flexibilitat horària</p>
-              <p>Publicat fa 5 dies</p>
-              <button class="btn btn-primary">Aplicar</button>
-            </div>
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/ux.jpg" alt="girl on the sofa whit a computer">
-          </div>
-
-          <div class="card">
-            <div class="card-body__text">
-              <h3>Lideratge en Marketing</h3>
-              <p>Jornada completa</p>
-              <p>Publicat fa 1 dies</p>
-              <button class="btn btn-primary">Comença</button>
-            </div>
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/marketing.jpg" alt="reunion of workmates at the office">
-          </div>
-
-          <div class="card">
-            <div class="card-body__text">
-              <h3>Especialista RRHH</h3>
-              <p>Jornada partida</p>
-              <p>Publicat fa 4 dies</p>
-              <button class="btn btn-primary">Més Detalls</button>
-            </div>
-              <img class="card-body__image" src="src/assets/img/jobs-desktop/hr.jpg" alt="empty office with sofas">
-          </div>
-
-      </section>
+        </section>
       </div>
     `;
 }
